71d01bfbc4ba07df4d1753b7aa4f86f33a3d5377
Daniel Kunicki <dankunicki@gmail.com>
2018-01-27 16:54:34 +0100

    add classes and modify input manager for gun powerups

---
+++ Assets/PowerUpMachineGun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpMachineGun : PowerUp {
+
+    protected override void Activate(PlayerControler player)
+    {
+        base.Activate(player);
+        player.useGun = false;
+    }
+
+}
