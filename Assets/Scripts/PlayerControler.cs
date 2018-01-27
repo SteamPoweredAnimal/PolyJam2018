be2f6210abbfad6a95551102c92e14a36b5a2fc6
Łukasz Biel <lukasz.biel@anixe.pl>
2018-01-27 10:36:07 +0100

    Rotation works

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -11,13 +11,14 @@ public class PlayerControler : MonoBehaviour
 	{
 		rigidbody2D = GetComponent<Rigidbody2D>();
 	}
-	
+
 	private void Update()
 	{
 		var translation = new Vector2(InputController.GetXMoveAxis(), InputController.GetYMoveAxis()) * Speed;
 		rigidbody2D.velocity = translation;
-		
-		var look = new Vector2(InputController.GetXLookAxis(), InputController.GetYLookAxis());
+
+		var look = new Vector3(0, 0, Mathf.Atan2(-InputController.GetXLookAxis(), -InputController.GetYLookAxis()) * Mathf.Rad2Deg);
 		transform.rotation = Quaternion.Euler(look);
+
 	}
 }
