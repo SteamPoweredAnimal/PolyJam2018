f132f79f1ef002464a6c85329a7a73c59f24d8e2
Daniel Kunicki <dankunicki@gmail.com>
2018-01-27 16:39:50 +0100

    Add walls, logic gates and fix bug: powerups colliding with missiles

---
+++ Assets/Scripts/LogicGate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicGate : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Missile missile = collider.GetComponent<Missile>();
+
+        if (missile != null)
+        {
+            
+            Missile.MissileType newType = missile.type == Missile.MissileType.False ? Missile.MissileType.True : Missile.MissileType.False;
+
+            Sprite newSprite;
+            switch (newType)
+            {
+                case Missile.MissileType.False:
+                    newSprite = missile.falseSprite;
+                    break;
+                case Missile.MissileType.True:
+                    newSprite = missile.trueSprite;
+                    break;
+                default:
+                    newSprite = missile.spriteRenderer.sprite;
+                    break;
+            }
+            missile.type = newType;
+            missile.spriteRenderer.sprite = newSprite;
+        }
+    }
+}
