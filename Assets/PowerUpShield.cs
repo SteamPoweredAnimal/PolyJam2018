c0ce575ee2cbedb09ecf4449d8049652a86ad0f2
Daniel Kunicki <dankunicki@gmail.com>
2018-01-27 15:51:24 +0100

    Add shield powerup

---
+++ Assets/PowerUpShield.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpShield : PowerUp
+{
+
+    public GameObject shieldPrefab;
+
+    protected override void Activate(PlayerControler player)
+    {
+        base.Activate(player);
+        if (shieldPrefab != null)
+        {
+            GameObject shieldGO = Instantiate(shieldPrefab, player.transform);
+            shieldGO.transform.localPosition = new Vector3(0, 0, 0.01f);
+        }
+    }
+
+}
