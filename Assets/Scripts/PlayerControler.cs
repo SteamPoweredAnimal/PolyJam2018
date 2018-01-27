687e588f2c8d1708f70fa7be0dddfa7ce2d43320
Łukasz Biel <lukasz.biel@anixe.pl>
2018-01-27 23:59:24 +0100

    merge

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -42,8 +42,6 @@ public class PlayerControler : MonoBehaviour
         }
             rigidbody2D.velocity = rigidbody2D.velocity* 5f;
             dashTimer = 0f;
-		var translation = new Vector2(InputController.GetXMoveAxis(), InputController.GetYMoveAxis()) * Speed;
-		rigidbody2D.velocity = translation;
        
 		var look = new Vector3(0, 0, Mathf.Atan2(InputController.GetXLookAxis(), -InputController.GetYLookAxis()) * Mathf.Rad2Deg);
         Debug.Log(look.magnitude);
