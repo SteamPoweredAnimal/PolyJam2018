d769e8f7d4823347865712b20c63a0cd20d0033d
Daniel Kunicki <dankunicki@gmail.com>
2018-01-27 18:19:10 +0100

    Add sparks when missiles collide with something

---
+++ Assets/Spark.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spark : MonoBehaviour {
+
+    float lifespan = 0.25f;
+    float timer = 0f;
+
+	// Update is called once per frame
+	void Update () {
+		if (timer >= lifespan)
+        {
+            Destroy(gameObject);
+        }
+        timer += Time.deltaTime;
+	}
+}
