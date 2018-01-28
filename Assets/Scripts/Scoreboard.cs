559a09ecf720b6303476daacaea57e04c310cdb3
adam-andrzejczak <archound@gmail.com>
2018-01-28 13:33:36 +0100

    aaasdasdasd

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -4,29 +4,27 @@ using UnityEngine.UI;
 
 public class Scoreboard : MonoBehaviour
 {
-    private Dictionary<string, Text> scores;
-    private Dictionary<string, int> points;
+    private Text scores;
+    private int points;
     
     private void Start()
-    {
-        scores = new Dictionary<string, Text>();
-        points = new Dictionary<string, int>();
+    {      
+            var text = transform.Find("Stats").Find("Row1").Find("Score").GetComponent<Text>();
 
-        var name = transform.Find("Stats").Find("Row1").Find("Name").GetComponent<Text>().text;
-        var text = transform.Find("Stats").Find("Row1").Find("Score").GetComponent<Text>();
+            scores = text;
+            points = 0;
 
-        scores.Add(name, text);
-        points.Add(name, 0);
 
+        Debug.Log("name: " + name);
     }
 
     public void RemovePoint(string missile)
     {
-        scores[missile].text = (--points[missile]).ToString();
+        scores.text = (--points).ToString();
     }
 
-    public void AddPoint(string missile)
+    public void AddPoint()
     {
-        scores[missile].text = (++points[missile]).ToString();
+        scores.text = (++points).ToString();
     }
 }
